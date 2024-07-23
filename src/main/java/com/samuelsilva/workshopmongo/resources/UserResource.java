ec58f2987fd5850a95bdcd3185d037b4fdc0766e
samuel1245678 <samuel89g3@hotmail.com>
2024-07-23 17:03:11 -0300

    Obtendo um usuario por id

--- src/main/java/com/samuelsilva/workshopmongo/resources/UserResource.java
+++ src/main/java/com/samuelsilva/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,12 @@ public class UserResource {
 		
 		//PARA RETURNAR A LISTA
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		//PARA RETURNAR A LISTA
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
--- src/main/java/com/samuelsilva/workshopmongo/services/UserService.java
+++ src/main/java/com/samuelsilva/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.samuelsilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.samuelsilva.workshopmongo.domain.User;
 import com.samuelsilva.workshopmongo.repository.UserRepository;
+import com.samuelsilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	// FINDALL RESPONSAVEL POR RETORNAR TODOS USUARIOS DO MEU BANCO
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		//User user = repo.findById(id);
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
